23319a433e2f6b8b9cbf1f184d786c40ccf8a20c
ncidorowa <166540449+ncidorowa@users.noreply.github.com>
2024-08-19 22:09:22 +0400

    Add files via upload

---
+++ modul_14_1.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balanse INTEGER NOT NULL
+)
+''')
+cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
+for i in range(1, 11):
+    cursor.execute('INSERT INTO Users (username, email, age, balanse) VALUES(?, ?, ?, ?)', (f'User{i}',f'example{i}@gmail.com',i*10, 1000))
+
+# for i in range(1, 11, 2):
+#     cursor.execute('UPDATE  Users SET balanse = ? WHERE username = ?', (500, f'User{i}'))
+#
+# for i in range(1, 11, 3):
+#     cursor.execute('DELETE FROM Users WHERE username = ?', (f'User{i}',))
+
+cursor.execute('SELECT * FROM Users WHERE age !=?', (60,))
+result = cursor.fetchall()
+
+for user in result:
+    print(f'Имя: {user[1]}| Почта: {user[2]})| Возраст: {user[3]}| Баланс:{user[4]}')
+
+
+
+connection.commit()
+connection.close()
