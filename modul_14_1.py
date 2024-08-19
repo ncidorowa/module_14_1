71ac319650fb4ff77bc52b0e34823dc06fa4b124
ncidorowa <166540449+ncidorowa@users.noreply.github.com>
2024-08-19 22:10:54 +0400

    Update modul_14_1.py

--- modul_14_1.py
+++ modul_14_1.py
@@ -16,11 +16,11 @@ cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
 for i in range(1, 11):
     cursor.execute('INSERT INTO Users (username, email, age, balanse) VALUES(?, ?, ?, ?)', (f'User{i}',f'example{i}@gmail.com',i*10, 1000))
 
-# for i in range(1, 11, 2):
-#     cursor.execute('UPDATE  Users SET balanse = ? WHERE username = ?', (500, f'User{i}'))
-#
-# for i in range(1, 11, 3):
-#     cursor.execute('DELETE FROM Users WHERE username = ?', (f'User{i}',))
+for i in range(1, 11, 2):
+    cursor.execute('UPDATE  Users SET balanse = ? WHERE username = ?', (500, f'User{i}'))
+    
+ for i in range(1, 11, 3):
+     cursor.execute('DELETE FROM Users WHERE username = ?', (f'User{i}',))
 
 cursor.execute('SELECT * FROM Users WHERE age !=?', (60,))
 result = cursor.fetchall()
